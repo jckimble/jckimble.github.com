b5ea2d71970ce53cdfff6f2a5ddc3a9511c8bb63
James C Kimble <me@jckimble.com>
2022-07-13 03:15:13 -0500

    try microdata

--- src/index.js
+++ src/index.js
@@ -7,43 +7,6 @@ import vCard from 'meta-data-vcard';
 
 require("./style.scss")
 
-
-/*const generateJSONLD=(data)=>{
-    const jsonld={
-        "@context": "https://schema.org",
-        "@type": "Organization",
-		"url": data.url,
-	    "name": data.name,
-		"logo": data.photo,
-		"image": data.photo,
-        "email": data.email,
-        "telephone": data.phone,
-        "jobTitle": data.title,
-        "address":{
-            "addressCountry": "USA",
-            "postalCode": "39601",
-            "addressRegion": "MS",
-            "addressLocality": "Brookhaven",
-        },
-		"contactPoint": {
-		    "@type": "ContactPoint",
-		    "email": data.email,
-            "telephone": data.phone
-		},
-		"description": data.description,
-		"sameAs": [
-		    data.github,
-		    data.codepen,
-		    data.linkedin,
-			data.twitter
-		]
-    }
-    const script = document.createElement('script');
-    script.setAttribute('type', 'application/ld+json');
-    script.textContent = JSON.stringify(jsonld);
-    document.head.appendChild(script);    
-}*/
-
 const vcard=new vCard();
 const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
