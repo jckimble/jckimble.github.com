30b753be0f175b3bcdecfbb8bcf8b48e242fdf41
James C Kimble <me@jckimble.com>
2022-07-13 02:52:46 -0500

    move vcard creator into its own repo

--- src/index.js
+++ src/index.js
@@ -3,31 +3,12 @@ import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 import QRCode from 'qrcode'
+import vCard from 'meta-data-vcard';
 
 require("./style.scss")
 
-const vcfToData=(text)=>{
-    return "data:text/vcard;base64,"+btoa(text)
-}
 
-const generateVCF=(vcfData)=>{
-    let content={
-        "BEGIN":"VCARD",
-        "VERSION":"2.1",
-        "N":[vcfData.familyName,vcfData.givenName,vcfData.middleName,vcfData.prefixName,vcfData.suffixName].join(";"),
-        "FN":vcfData.name,
-        "TEL;WORK":vcfData.phone,
-        "EMAIL;WORK":vcfData.email,
-        "TITLE":vcfData.title,
-        "URL":vcfData.url,
-        "NOTE":vcfData.description,
-        "PHOTO;JPEG":vcfData.photo,
-        "END":"VCARD"
-    }
-    return Object.entries(content).map(([k,v])=>k+":"+v).join("\n")
-}
-
-const generateJSONLD=(data)=>{
+/*const generateJSONLD=(data)=>{
     const jsonld={
         "@context": "https://schema.org",
         "@type": "Organization",
@@ -61,42 +42,34 @@ const generateJSONLD=(data)=>{
     script.setAttribute('type', 'application/ld+json');
     script.textContent = JSON.stringify(jsonld);
     document.head.appendChild(script);    
-}
-    
-const getVCardData=()=>{
-    let data={}
-    const vcardData=document.querySelectorAll('[data-vcard]');
-    vcardData.forEach((item)=>{
-        let name=item.getAttribute("data-vcard")
-        switch(item.tagName){
-            case "A":
-                let href=item.getAttribute("href")
-                if(href.startsWith("mailto:") || href.startsWith("tel:")){
-                    data[name]=href.substring(href.indexOf(':') + 1)
-                    return
-                }
-                data[name]=href
-                break;
-            case "IMG":
-                data[name]=item.getAttribute("src")
-                break;
-            default:
-                data[name]=item.textContent
+}*/
+
+const vcard=new vCard();
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if(entry.intersectionRatio > 0){
+            let data=vcard.String({
+                embedImgs:false
+            })
+            QRCode.toCanvas(document.querySelector("#qrcode canvas"),data,{
+                version:"Auto",
+                errorCorrectionLevel:"Medium",
+                margin:4,
+                color:{
+                    light:"#FFF",
+                    dark:"#000",
+                },
+                toSJISFunc: QRCode.toSJIS
+            })
         }
-    })
-    return data
+    });
+});
+window.downloadVcf=()=>{
+    var link = document.createElement("a");
+    link.setAttribute("download","jckimble.vcf")
+    link.setAttribute("href",vcard.toDataURL({embedImgs:true}))
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
 }
-const vCardData=getVCardData()
-generateJSONLD(vCardData)
-const vcfData=generateVCF(vCardData)
-document.querySelector("#vcf").setAttribute("href",vcfToData(vcfData))
-QRCode.toCanvas(document.querySelector("#qrcode canvas"),vcfData,{
-    version:"Auto",
-    errorCorrectionLevel:"Medium",
-    margin:4,
-    color:{
-        light:"#FFF",
-        dark:"#000",
-    },
-    toSJISFunc: QRCode.toSJIS
-})
+observer.observe(document.querySelector("#qrcode"));
