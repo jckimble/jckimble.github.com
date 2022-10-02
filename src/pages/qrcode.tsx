52504642f117d89a67d692e4943a2916aa96cbcb
James C Kimble <me@jckimble.com>
2022-10-02 12:57:01 -0500

    feat: add qrcode page

--- src/pages/qrcode.tsx
+++ src/pages/qrcode.tsx
@@ -12,8 +12,8 @@ function Home() {
         <header>
           <ul>
             <li>
-              <Link to="qrcode" data-tooltip="QrCode">
-                <FontAwesomeIcon icon={["fas", "qrcode"]} />
+              <Link to="/" data-tooltip="Home">
+                <FontAwesomeIcon icon={["fas", "user"]} />
               </Link>
             </li>
             <li>
@@ -26,25 +26,7 @@ function Home() {
               </a>
             </li>
           </ul>
-          <a className="profile-img" href="https://jckimble.com/">
-            <img
-              src="https://0.gravatar.com/avatar/43799da335050c4cebcc859ef15dd939?s=150"
-              alt="James C Kimble"
-              data-vcard="photo"
-            />
-          </a>
-          <h1>James C Kimble</h1>
-          <h2>Software Consultant</h2>
-          <p>
-            James is a passionate software engineer &amp; pool player that
-            dedicates most of his time between automating everything he can and
-            playing pool.
-          </p>
-          <p>
-            His primary focus is in security and automation. Even the best
-            written and looking sites are worthless if they are easily hacked.
-            The human element is always the weakest attack point.
-          </p>
+          <img src="./qr-code.svg" height={500} />
         </header>
         <footer>
           <ul>
